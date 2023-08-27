6d81010a6216433874536412304f2e40bcedf7ea
c.bushehry <c.bushehry@gmail.com>
2023-08-27 14:20:03 -0700

    added alien1&2 / fixed background

--- js/game.js
+++ js/game.js
@@ -3,23 +3,28 @@ let gameScene = new Phaser.Scene('Game');
 gameScene.preload = function(){
   this.load.image('background', 'assets/images/background.png');
   this.load.image('player', 'assets/images/playerShip.png');
+  this.load.image('alien', 'assets/images/alienShip.png');
 };
 
 gameScene.create = function() {
   let bg = this.add.sprite(0, 0, 'background');
- 
-  bg.setPosition(1280/2, 720/2);
- 
-  let gameW = this.sys.game.config.width;
-  let gameH = this.sys.game.config.height;
+  bg.setOrigin(0, 0);
   
-  let player = this.add.sprite(70, 180, 'player');
+  let player = this.add.sprite(140, 1847/2, 'player');
+  player.setScale(0.6, 0.6);
+
+  let alien1 = this.add.sprite(4700, 930, 'alien');
+  alien1.setScale(0.8, 0.8);
+
+  let alien2 = this.add.sprite(4700, 740, 'alien');
+  alien2.setScale(0.8, 0.8);
+  
 };
  
 let config = {
   type: Phaser.AUTO, // Phaser will use WebGL if available, if not it will use Canvas
-  width: 1280,
-  height: 720,
+  width: 5119,
+  height: 1847,
   scene: gameScene
 };
  
