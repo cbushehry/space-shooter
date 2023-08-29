aa2b9e0e6e8ddde775046a7db96e311559af0a2f
Cameron Bushehry <c.bushehry@gmail.com>
2023-08-29 10:35:24 -0700

    refactored sprite positions/camera follows playerShip v1

--- js/game.js
+++ js/game.js
@@ -10,24 +10,28 @@ gameScene.preload = function() {
 };
 
 gameScene.create = function() {
-  let bg = this.add.sprite(0, 0, 'background');
-  bg.setOrigin(0, 0);
+  let bg = this.add.sprite(0, 5119, 'background');
+  bg.setOrigin(0, 1);
   
   //PLAYER SPRITE
-  this.player = this.add.sprite(140, 1847/2, 'player');
+  this.player = this.add.sprite(1847/2, 4819, 'player');
   this.player.setScale(0.7);
 
   //ALIEN SPRITES
-  this.alien1 = this.add.sprite(4700, 1130, 'alien1');
+  this.alien1 = this.add.sprite(1130, 4700, 'alien1');
   this.alien1.setScale(0.8);
-  this.alien2 = this.add.sprite(4700, 540, 'alien2');
+  this.alien2 = this.add.sprite(540, 4700, 'alien2');
   this.alien2.setScale(0.8);
   
   //ASTEROID SPRITES
-  this.asteroid1 = this.add.sprite(3150, 340, 'asteroid1');
+  this.asteroid1 = this.add.sprite(350, 3140, 'asteroid1');
   this.asteroid1.setScale(0.7);
-  this.asteroid2 = this.add.sprite(2350, 1300, 'asteroid2');
+  this.asteroid2 = this.add.sprite(1350, 2300, 'asteroid2');
   this.asteroid2.setScale(0.7);
+
+  // CAMERA SETTINGS
+  this.cameras.main.startFollow(this.player);
+  this.cameras.main.setBounds(0, 0, 1847, 5119);
 };
 
 gameScene.update = function(time, delta) {
@@ -52,7 +56,7 @@ gameScene.update = function(time, delta) {
     player.x += speed * delta / 1000;
   }
 
-// check for active input then walk (change this to shoot lasers)
+  /* check for active input then walk (change this to shoot lasers)
    if(this.input.activePointer.isDown) {
      this.player.x += speed * delta / 1000;
   }  
@@ -64,13 +68,13 @@ gameScene.update = function(time, delta) {
     console.log('you Crashed!');
       this.scene.restart();
       return;
-    }
+    } */
 };
 
 let config = {
   type: Phaser.AUTO,
-  width: 1847,
-  height: 5119,
+  width: 1570,
+  height: 769,
   scene: gameScene
 };
 
