04a327ecf8a5367b0b1697b20a9061b635df5370
Cameron Bushehry <c.bushehry@gmail.com>
2023-08-31 10:22:38 -0700

    refactored comments v2

--- js/game.js
+++ js/game.js
@@ -1,10 +1,13 @@
+// Creating a new game scene
 let gameScene = new Phaser.Scene('Game');
 
+// Define key codes for easy reference
 const KEY_W = Phaser.Input.Keyboard.KeyCodes.W;
 const KEY_A = Phaser.Input.Keyboard.KeyCodes.A;
 const KEY_S = Phaser.Input.Keyboard.KeyCodes.S;
 const KEY_D = Phaser.Input.Keyboard.KeyCodes.D;
 
+// Define constants for game settings
 const BG_WIDTH = 1847;
 const BG_HEIGHT = 1706;
 const PLAYER_INITIAL_SCALE = 0.4;
@@ -12,17 +15,17 @@ const PLAYER_INITIAL_ANGLE = 270;
 const PLAYER_SPEED = 500;
 const BG_SCROLL_SPEED = 10;
 
+// Preload assets
 gameScene.preload = function() {
   this.load.image('background', 'assets/images/background.png');
   this.load.image('player', 'assets/images/playerShip.png');
 };
 
+// Create game objects and initialize settings
 gameScene.create = function() {
-  // BACKGROUND SPRITE grid size
+  // Initialize background grid
   const cols = 3;
   const rows = 3;
-
-  // Initialize background sprites in a loop
   this.bgSprites = [];
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
@@ -33,33 +36,34 @@ gameScene.create = function() {
     }
   }
 
-  // Initialize keyboard inputs and store them as properties
+  // Initialize keyboard inputs
   this.keyW = this.input.keyboard.addKey(KEY_W);
   this.keyA = this.input.keyboard.addKey(KEY_A);
   this.keyS = this.input.keyboard.addKey(KEY_S);
   this.keyD = this.input.keyboard.addKey(KEY_D);
 
-  //PLAYER SPRITE
+  // Initialize player sprite
   this.player = this.add.sprite(BG_WIDTH / 2, BG_HEIGHT / 2, 'player');
   this.player.setScale(PLAYER_INITIAL_SCALE);
-  this.player.setOrigin(0.4, 0.5);  // setOrigin(0.4, 0.5) so playerShip spins from the thruster instead of middle of spaceship
+  this.player.setOrigin(0.4, 0.5);  //player.setOrigin(0.4, 0.5) so playerShip spins from thrusters
   this.player.angle = PLAYER_INITIAL_ANGLE;
 
-  // CAMERA SETTINGS
+  // Initialize camera settings
   this.cameras.main.startFollow(this.player);
   this.cameras.main.setBounds(0, 0, BG_WIDTH, BG_HEIGHT);
   const zoomFactor = Math.min(config.width / BG_WIDTH, config.height / BG_HEIGHT);
   this.cameras.main.setZoom(zoomFactor);
 
-  const cameraPadding = 0; // Adjust based on your needs
+  const cameraPadding = 0;
   this.cameras.main.setBounds(
-    cameraPadding, 
-    cameraPadding, 
-    BG_WIDTH - cameraPadding * 2, 
+    cameraPadding,
+    cameraPadding,
+    BG_WIDTH - cameraPadding * 2,
     BG_HEIGHT - cameraPadding * 2
   );
 };
 
+// Update game objects
 gameScene.update = function(time, delta) {
   let pointer = this.input.activePointer;
   let bgScrollSpeed = BG_SCROLL_SPEED;
@@ -68,43 +72,30 @@ gameScene.update = function(time, delta) {
   let dx = 0;
   let dy = 0;
 
-  // You can directly use this.keyW, this.keyA, this.keyS, this.keyD
-  if (this.keyW.isDown) {
-    dy = -speed * delta / 1000;
-  }
-  if (this.keyA.isDown) {
-    dx = -speed * delta / 1000;
-  }
-  if (this.keyS.isDown) {
-    dy = speed * delta / 1000;
-  }
-  if (this.keyD.isDown) {
-    dx = speed * delta / 1000;
-  }
+  // Check for keyboard inputs and update player position accordingly
+  if (this.keyW.isDown) dy = -speed * delta / 1000;
+  if (this.keyA.isDown) dx = -speed * delta / 1000;
+  if (this.keyS.isDown) dy = speed * delta / 1000;
+  if (this.keyD.isDown) dx = speed * delta / 1000;
 
+  // Rotate player towards pointer if clicked
   if (pointer.isDown) {
-    let angle = Phaser.Math.Angle.Between(
-      player.x, player.y,
-      pointer.worldX, pointer.worldY
-    );
+    let angle = Phaser.Math.Angle.Between(player.x, player.y, pointer.worldX, pointer.worldY);
     player.rotation = angle;
   }
 
-  // Update background positions
+  // Update and loop background positions
   this.bgSprites.forEach(bg => {
     bg.x -= dx;
     bg.y -= dy - bgScrollSpeed * delta / 1000;
-
-    // Loop background horizontally
     if (bg.x > BG_WIDTH) bg.x -= BG_WIDTH * 3;
     if (bg.x < -BG_WIDTH) bg.x += BG_WIDTH * 3;
-
-    // Loop background vertically
     if (bg.y > BG_HEIGHT) bg.y -= BG_HEIGHT * 3;
     if (bg.y < -BG_HEIGHT) bg.y += BG_HEIGHT * 3;
   });
 };
 
+// Game configuration
 let config = {
   type: Phaser.AUTO,
   scale: {
@@ -116,4 +107,5 @@ let config = {
   scene: gameScene,
 };
 
+// Create the game instance
 let game = new Phaser.Game(config);
